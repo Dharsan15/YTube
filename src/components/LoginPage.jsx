2931cd61f44dd99775a3853cbbc0f632b4e76424
Dharsan15 <dharsanravi6677@gmail.com>
2023-11-29 21:23:33 +0530

    new commit

---
+++ src/components/LoginPage.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Login = () => {
+    return (
+     <>
+
+	  <div>
+          
+             <input placeholder="Email" ></input>
+             <input placeholder="Password" ></input>
+
+          
+      </div>
+
+     </>
+
+    )
+}
+
+export default Login;
