b267d21829d481e4eb924adeb29fe01bbd56b6d0
Dharsan15 <dharsanravi6677@gmail.com>
2023-12-01 23:13:23 +0530

    new commit

--- src/components/Navbar2.jsx
+++ src/components/Navbar2.jsx
@@ -1,51 +1,47 @@
-import { Stack , Button } from "@mui/material";
+import { Stack, Button } from "@mui/material";
 import { Link } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
-import Avatar from '@mui/material/Avatar';
+import Avatar from "@mui/material/Avatar";
 import { useState } from "react";
-
 
 import { logo } from "../utils/constants";
 import { SearchBar } from "./";
 
-
-const Navbar2 = () => { 
-
-  const [toggle , settoggle] = useState(true);
+const Navbar2 = () => {
+  const [toggle, settoggle] = useState(true);
 
   const handleToggle = () => {
-     settoggle(!toggle);
-  }
-  
+    settoggle(!toggle);
+  };
+
   const navigate = useNavigate();
 
   const handleLogin = () => {
-     navigate('/login');
-  }
-  
+    navigate("/login");
+  };
+
   return (
-  
-
-  <Stack
-    direction="row"
-    alignItems="center"
-    p={2}
-    sx={{
-      position: "sticky",
-      background: "#000",
-      top: 0,
-      justifyContent: "space-between",
-    }}
-  >
-    <Link to="/" style={{ display: "flex", alignItems: "center" }}>
-      <img src={logo} alt="logo" height={45} />
-      <h2 className="title" style={{color : "white" }} >YTube</h2>
-    </Link>
-    <SearchBar />
-    <Avatar  style={{padding : 5 , backgroundColor : 'gray'}} > DH </Avatar>
-  </Stack>
-) 
-
+    <Stack
+      direction="row"
+      alignItems="center"
+      p={2}
+      sx={{
+        position: "sticky",
+        background: "#000",
+        top: 0,
+        justifyContent: "space-between",
+      }}
+    >
+      <Link to="/" style={{ display: "flex", alignItems: "center" }}>
+        <img src={logo} alt="logo" height={45} />
+        <h2 className="title" style={{ color: "white" }}>
+          YTube
+        </h2>
+      </Link>
+      <SearchBar />
+      <Avatar style={{ padding: 5, backgroundColor: "gray" }}> DH </Avatar>
+    </Stack>
+  );
 };
 
 export default Navbar2;
